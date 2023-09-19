9b0bfa2f4adf8e36a5cadccf3eea56550ac83b47
ahmadmazrua <ahmadmazrua@outlook.com>
2023-09-19 05:30:05 -0700

    my commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+        char ch = 'z';
+
+        while (ch >= 'a')
+        {
+                putchar (ch);
+                ch--;
+        }
+        putchar('\n');
+        return (0);
+}
